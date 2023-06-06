fc237dc3e6ff9276950e66435f26767f13f42936
Bedprakah Bhoi <bhoi.chintu@gmail.com>
2023-06-06 23:01:01 +0530

    Update q1.py

--- python/q1.py
+++ python/q1.py
@@ -1,25 +1,34 @@
-def highest_frequency_word_length(input_string):
-    # Split the string into words
-    words = input_string.split()
+def find_frequent_string_length(sent):
+  word_count = dict()
+  #frequent_words = []
 
-    # Count the frequency of each word
-    word_frequency = Counter(words)
-
-    # Find the highest frequency
-    max_frequency = max(word_frequency.values())
-
-    # Find the length of the highest-frequency word
-    highest_frequency_word_length = max(len(word) for word, freq in word_frequency.items() if freq== max_frequency)
-
-    return highest_frequency_word_length
+  #storing distinct word frequency into a dictionary
+  for word in sent.split():
+    if word in word_count.keys():
+      word_count[word] += 1
+    else:
+      word_count[word] = 1
+  #sorting the dictionary based on frequency of words
+  word_count = sorted(word_count.items(),key = lambda x:x[1],reverse=True)
+  #storing maximum occuring word frequency
+  max_count = word_count[0][1]
+  max_length=0 #for checking length of each word
+  for key,item in word_count:
+    if item == max_count:
+      if len(key) > max_length:
+        max_length = len(key)
+    else:
+      break
+  return max_length
+  #print(word_count)
 
 
 # Test_case_1
 str1 = "write write write all the number from from from"
-res1 = find_highest_frequency_word_length(input_str1)
-print("Length of the highest-frequency word in test case 1:", result1)
+res1 = find_frequent_string_length(str1)
+print("Length of the highest-frequency word in test case 1:", res1)
 
 # Test_case_2
 str2 = "hello hello world welcome welcome welcome to "
-res2 = find_highest_frequency_word_length(str1)
-print("Length of the highest-frequency word in test case 1:", result1)
+res2 = find_frequent_string_length(str2)
+print("Length of the highest-frequency word in test case 1:", res2)
